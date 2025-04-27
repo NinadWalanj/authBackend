b4e95906f31efb7eb66e2697aad31fae4a1200fb
Ninad Walanj <ninadwalanj@gmail.com>
2025-04-26 21:48:37 -0400

    second commit

--- app.js
+++ app.js
@@ -52,5 +52,5 @@ app.get("/", (req, res) => {
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on Port:${PORT}`);
 });
